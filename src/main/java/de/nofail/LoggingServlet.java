4e697ad6b49f69bb0239735b4d25595314520964
Peter Schröder <nofail@peter-schroders-macbook.local>
2010-03-30 17:53:33 +0200

    add launcher for eclipse, more docs, and running servlet

--- src/main/java/de/nofail/LoggingServlet.java
+++ src/main/java/de/nofail/LoggingServlet.java
@@ -1,23 +1,31 @@
 package de.nofail;
 
+
 import java.io.IOException;
 
-import javax.servlet.GenericServlet;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
 
 /**
  * @author nofail
  */
-public class LoggingServlet extends GenericServlet{
+public class LoggingServlet extends HttpServlet{
 
+	private static final Logger log = Logger.getLogger(LoggingServlet.class);
+	
 	private static final long serialVersionUID = 1L;
-
-	public void service(ServletRequest arg0, ServletResponse arg1)
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		
+		response.setContentType("text/html");
+		String html = String.format("<html><head><title>logging-servlet</title></head><body>You requested=[%s?%s]</body></html>",  request.getRequestURL(), request.getQueryString());
+		log.debug(html);
+		response.getOutputStream().println(html);
 	}
 
 }
