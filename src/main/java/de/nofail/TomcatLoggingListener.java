9c5976a662d7d8539742d747991ebb55d1c13ecd
Peter Schröder <nofail@peter-schroders-macbook.local>
2010-03-31 16:39:25 +0200

    add documentation and improve readme

---
+++ src/main/java/de/nofail/TomcatLoggingListener.java
@@ -0,0 +1,52 @@
+package de.nofail;
+
+import java.io.File;
+import java.net.URISyntaxException;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.xml.DOMConfigurator;
+
+/**
+ * Context listener for initializing the log4j watchdog.
+ * <p>
+ * This implementation depends on defaults:
+ * <ul>
+ * <li>a log4j configuration file with the name SERVLET_CONTEXT-log4j.xml must
+ * exist
+ * <li>the configuration file must be in the root folder of the classpath
+ * <li>the watchdog-thread looks for changes to the file every second
+ * </ul>
+ * <p>
+ * Configuration points are possible by adding servlet init-params.
+ * 
+ * @author nofail
+ */
+public class TomcatLoggingListener implements ServletContextListener {
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		System.out.println("Initializing the servet context!");
+		ServletContext servletContext = sce.getServletContext();
+		try {
+			String log4jFile = String.format("%s-log4j.xml", servletContext.getContextPath());
+			System.out.println("Trying to read log4jFile=" + log4jFile);
+			String configFilename = new File(getClass().getResource(log4jFile).toURI()).getAbsolutePath();
+			long delay = 1000;
+			System.out.println(String.format("Configuring watchdog on log4jFile=%s with delay=%s", configFilename, delay));
+			DOMConfigurator.configureAndWatch(configFilename, delay);
+		} catch (URISyntaxException e) {
+			throw new IllegalStateException("Error resolving log4j configuration file for context=" + servletContext, e);
+		}
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		System.out.println("Destroying the servet context!");
+		// this should not be necessary
+		Logger.getRootLogger().getLoggerRepository().shutdown();
+	}
+}
---
+++ src/main/java/de/nofail/TomcatLoggingServlet.java
@@ -0,0 +1,42 @@
+package de.nofail;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.liftweb.textile.TextileParser;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.log4j.Logger;
+
+/**
+ * Simple example of a servlet implementation.
+ * 
+ * @author nofail
+ */
+@SuppressWarnings("serial")
+public class TomcatLoggingServlet extends HttpServlet {
+
+	private static final Logger log = Logger.getLogger(TomcatLoggingServlet.class);
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		log.debug("responding to request=" + request);
+
+		String readme = FileUtils.readFileToString(new File("readme.textile"));
+		String fragment = "<html><head><title>tomcat-logging</title></head><body>You requested=[%s?%s] <hr> %s</body></html>";
+		String html = String.format(fragment, request.getRequestURL(), request.getQueryString(), TextileParser.toHtml(readme));
+
+		response.setContentType("text/html");
+		response.getOutputStream().println(html);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
